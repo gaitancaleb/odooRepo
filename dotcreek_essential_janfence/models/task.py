006e1dce2f6436bc61341503c5583d3ed63bba8c
Carlos <cquirosny@gmail.com>
2021-02-23 09:30:39 -0600

    [MOD] change on field service and drawing

--- dotcreek_essential_janfence/models/task.py
+++ dotcreek_essential_janfence/models/task.py
@@ -4,6 +4,12 @@ from odoo import fields, models, api
 class Task(models.Model):
     _inherit = "project.task"
 
+    stage_id_name = fields.Char(related='stage_id.name', readonly=True)
+    worksheet_template_id_name = fields.Char(related='worksheet_template_id.name', readonly=True)
+    user_id = fields.Many2one('res.users',
+                              string='Assigned to',domain=[('installers','=',True)],
+                              default=lambda self: self.env.uid,
+                              index=True, tracking=True)
     def create_email_start(self):
         template = self.env.ref('dotcreek_essential_janfence.mail_template_data_send_report')
         res = template.sudo().send_mail(self.id, force_send=True)
